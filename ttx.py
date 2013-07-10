3ca7a7cc9695be982b527f73dfff3a5774ef68dd
Thierry Carrez <thierry@openstack.org>
2013-07-10 14:41:41 +0200

    Havana-style ttx
    This new version cares about the stuff ttx cares about now, especially
    with the new blueprint tracking style enabled.

--- ttx.py
+++ ttx.py
@@ -5,15 +5,34 @@ import sys
 from launchpadlib.launchpad import Launchpad
 
 
-def show_bp(bp, issues):
-    if issues:
-        msg = "%s (%s" % (bp.name, bp.priority)
-        if bp.assignee:
-            msg = "%s, %s" % (msg, bp.assignee.name)
-        if bp.milestone:
-            msg = "%s, %s" % (msg, bp.milestone.name)
-        print "%s)\n%s\n%s" % (msg, bp.web_link, issues)
+def show_bp(bp, depends=False):
+    global warning_count
 
+    if bp.assignee:
+        assignee = bp.assignee.name
+    else:
+        assignee = 'No assignee'
+
+    if bp.milestone:
+        milestone = bp.milestone.name
+    else:
+        milestone = 'No milestone'
+
+    if depends:
+        prefix = "  <-"
+    else:
+        warning_count += 1
+        prefix = "*"
+
+
+    print "%s %s (%s, %s, %s)\n%s %s" % (
+        prefix,
+        bp.name,
+        bp.priority,
+        assignee,
+        milestone,
+        " " * len(prefix),
+        bp.web_link)
 
 # Parse arguments
 if len(sys.argv) < 3:
@@ -27,51 +46,137 @@ prio = ["Not", "Undefined", "Low", "Medium", "High", "Essential"]
 # Log into LP
 lp = Launchpad.login_anonymously('bp-issues', 'production', version='devel')
 
-# Retrieve project, series and corresponding blueprints
+# Retrieve project, series and milestones
+warning_count = 0
 blueprints = []
 project = lp.projects[projectname]
 series = project.getSeries(name=seriesname)
+future_milestones = []
+current_milestone = None
+for ms in series.active_milestones:
+    next_milestone = current_milestone
+    future_milestones.append(next_milestone)
+    current_milestone = ms
+
+implemented = under_review = in_progress = not_started = 0
+next_count = 0
+needs_assignee = []
+needs_triage = []
+unknown_status = []
+depends_issues = []
+already_implemented = []
+extra_triage = []
+
 for bp in series.valid_specifications:
-    if not bp.is_complete:
-        blueprints.append(bp)
+    # Active milestone
+    if bp.milestone == current_milestone:
+        if bp.is_complete:
+            if bp.implementation_status == 'Implemented':
+                implemented += 1
+            continue
+        if prio.index(bp.priority) > 2:
+            if bp.implementation_status == 'Needs Code Review':
+                under_review += 1
+            else:
+                if bp.implementation_status in ('Unknown', 'Not started'):
+                    not_started += 1
+                else:
+                    in_progress += 1
 
-# Get the milestones for the series
-milestones = []
-for ms in series.active_milestones_collection:
-    milestones.append(ms)
-
-# Find targeted blueprints that are not in the series
-for bp in project.valid_specifications:
-    if bp.milestone in milestones:
-        if bp not in blueprints:
-            show_bp(bp,
-                "* Not in series goal while targeted to a series milestone\n")
-
-for bp in blueprints:
-    issues = ""
-    # No assignee
-    if bp.milestone:
+        # No assignee
         if not bp.assignee:
-            issues += "* Targeted to a milestone but has no assignee\n"
+            needs_assignee.append(bp)
+
+        # No priority
+        if prio.index(bp.priority) < 2:
+            needs_triage.append(bp)
+
+        # Unknown status
         if bp.implementation_status == "Unknown":
-            issues += "* Targeted to a milestone but unknown status\n"
+            unknown_status.append(bp)
 
-    # No priority
-    if prio.index(bp.priority) < 2:
-        issues += "* No priority\n"
+        # Dependencies
+        for dep in bp.dependencies:
+            if not dep.is_complete:
+                if (prio.index(bp.priority) > prio.index(dep.priority) or not
+                    dep.milestone):
+                    depends_issues.append((bp, dep))
 
-    # Essential and no target
-    if bp.priority == "Essential" and not bp.milestone:
-        issues += "* Essential but not targeted to a milestone\n"
+        continue
 
-    # Dependencies
-    for dep in bp.dependencies:
-        if not dep.is_complete:
-            if dep not in blueprints and dep.target == project:
-                issues += "* Depends on blueprint that is not in plan"
-                issues += " (%s)\n" % dep.name
-            if prio.index(bp.priority) > prio.index(dep.priority):
-                issues += "* Depends on blueprint with lower priority"
-                issues += " (%s)\n" % dep.name
+    if bp.milestone == next_milestone:
+        # Next milestone count
+        if prio.index(bp.priority) > 2:
+            next_count += 1
+        # Extra triage
+        if bp.priority == 'Undefined':
+            extra_triage.append(bp)
 
-    show_bp(bp, issues)
+    if bp.milestone in future_milestones:
+        # Early implementations
+        if bp.implementation_status == 'Implemented':
+            already_implemented.append(bp)
+
+critical_bugs = []
+open_critical = project.searchTasks(importance='Critical')
+for bugtask in open_critical:
+    if (bugtask.status != 'Fix Committed' and
+        bugtask.milestone != current_milestone):
+        critical_bugs.append(bugtask)
+
+# Report
+total = implemented + under_review + in_progress + not_started
+print "Tracking %d blueprints for %s:" % (total, current_milestone.name)
+print "%d%% done, %d%% under review, %d%% in progress, %d%% not started" % (
+    int(implemented*100/total),
+    int(under_review*100/total),
+    int(in_progress*100/total),
+    int(not_started*100/total))
+print
+if next_milestone:
+    print "Next milestone currently has %d targeted blueprints" % next_count
+    print
+
+if needs_triage:
+    print "Needs triaging:"
+    for bp in needs_triage:
+        show_bp(bp)
+    print
+
+if needs_assignee:
+    print "Needs an assignee:"
+    for bp in needs_assignee:
+        show_bp(bp)
+    print
+
+if unknown_status:
+    print "Needs a status update:"
+    for bp in unknown_status:
+        show_bp(bp)
+    print
+
+if depends_issues:
+    print "Dependency issues:"
+    for bp, dep in depends_issues:
+        show_bp(bp)
+        show_bp(dep, depends=True)
+    print
+
+if already_implemented:
+    print "In a future milestone but already implemented:"
+    for bp in already_implemented:
+        show_bp(bp)
+    print
+
+if critical_bugs:
+    print "Some critical bugs are not targeted to the current milestone:"
+    for bugtask in critical_bugs:
+        print "* %s" % bugtask.bug.web_link
+        warning_count += 1
+    print
+
+if warning_count < 3 and extra_triage:
+    print "You may consider triaging next milestone blueprints:"
+    for bp in extra_triage:
+        show_bp(bp)
+    print
